c759cc8d074562a9231e336a283728f6b6a410d3
Greg Griffin <ggriffin84@gmail.com>
2016-12-19 21:45:16 -0800

    Update colorifyPlugin.js

--- colorifyPlugin.js
+++ colorifyPlugin.js
@@ -1,28 +1,35 @@
 
 
+$.fn.colorify = function(property) {
 
-
-
-
-	$.fn.colorify = function() {
-
-		 var mathly = '#'+'0123456789abcdef'.split('').map(function(v,i,a){
+	
+	 var mathly = '#'+'0123456789abcdef'.split('').map(function(v,i,a){
 	  					return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
-	    	
-	    	this.css( "background-color", mathly );
+	 this.css( property, mathly );
+	
+}
 
-		};
+
+
+// 	$.fn.colorify = function() {
+
+// 		 var mathly = '#'+'0123456789abcdef'.split('').map(function(v,i,a){
+// 	  					return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
+	    	
+// 	    	this.css( "background-color", mathly );
+
+// 		};
 	 
 
 
-	$.fn.colorifyText = function() {
+// 	$.fn.colorifyText = function() {
 
-		 var mathly = '#'+'0123456789abcdef'.split('').map(function(v,i,a){
-	  					return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
+// 		 var mathly = '#'+'0123456789abcdef'.split('').map(function(v,i,a){
+// 	  					return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
 	    	
-	    	this.css( "color", mathly );
+// 	    	this.css( "color", mathly );
 
-		};
+// 		};
 	 
 
 
